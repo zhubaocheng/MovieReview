4468f6de61831ab6127d889662256aa54cbd485d
zhubaocheng <imzhubaocheng@gmail.com>
2017-06-17 13:23:12 +0800

    add review and hook movie/user relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   #validates :title, :description, presence: true
   belongs_to :user
+  has_many :reviews
   validates_presence_of :title, :description
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616133844) do
+ActiveRecord::Schema.define(version: 20170617051421) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170616133844) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "movie_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
